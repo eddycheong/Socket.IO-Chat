f5dd972d5a20a3aa9d832973e394ea0902a11caa
MisterPanda <cheong.e49@gmail.com>
2015-09-19 19:39:44 -0700

    Append the user messages directly to the message list and identity it by their own color.

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ io.on('connection', function(socket){
 
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-		io.emit('chat message', msg)
+		socket.broadcast.emit('chat message', msg)
 	});
-	
+
 	socket.on('disconnect', function(){
 		console.log(users[socket.id] +' disconnected');
 		io.emit('chat message', users[socket.id] + ' disconnected');
